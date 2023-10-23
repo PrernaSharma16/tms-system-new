a1e9729f93f2128e7c4fa1fe8d6099ba741a113e
Yash41299 <yashgoley99@gmail.com>
2023-10-23 17:43:16 +0530

    23/10/2023-yash

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginHeaderComponent } from './login-header/login-header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LoginBodyComponent } from './login-body/login-body.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { LoginFooterComponent } from './login-footer/login-footer.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { StudentViewComponent } from './student-view/student-view.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginHeaderComponent,
+    LoginBodyComponent,
+    LoginFooterComponent,
+    CreateUserComponent,
+    StudentViewComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/login-body/login-body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-body',
+  templateUrl: './login-body.component.html',
+  styleUrls: ['./login-body.component.css']
+})
+export class LoginBodyComponent {
+
+}
