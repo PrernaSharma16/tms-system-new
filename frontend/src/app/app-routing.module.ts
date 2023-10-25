e41edeca0423bf144febc27add7372bfa786c5e8
Yash41299 <yashgoley99@gmail.com>
2023-10-25 16:14:31 +0530

    25/10/2023-yash

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { LoginBodyComponent } from './login-body/login-body.component';
 import { TeacherViewComponent } from './teacher-view/teacher-view.component';
+import { StudentViewComponent } from './student-view/student-view.component';
 
 const routes: Routes = [
   {path:'',component:LoginBodyComponent},
   {path:'create',component:CreateUserComponent},
-  {path:'teacher',component:TeacherViewComponent}
+  {path:'teacher',component:TeacherViewComponent},
+  {path:'student',component:StudentViewComponent}
 ];
 
 @NgModule({
