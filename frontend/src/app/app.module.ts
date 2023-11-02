63b4c3ddc532f46c8f03805ef9e8bd6b0483f3a4
Yash41299 <yashgoley99@gmail.com>
2023-11-02 15:28:30 +0530

    2/11/2023-yash

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { SampleComponent } from './sample/sample.component';
 import { MatButtonModule } from '@angular/material/button';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { LoadingComponent } from './loading/loading.component';
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
     StudentViewComponent,
     TeacherViewComponent,
     SampleComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    LoadingComponent
     
   ],
   imports: [
